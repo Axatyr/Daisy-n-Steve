92f9e3e8f429da7c3dd2fc82fac297fed942f426
Alessia Lombardi <alessia.lombardi4@studio.unibo.it>
2021-11-09 19:43:48 +0100

    Modify methods costruisci_cielo, costruisci_prato, costruisci_sole

--- Daisy 'n' Steve/Figura.cpp
+++ Daisy 'n' Steve/Figura.cpp
@@ -1,5 +1,7 @@
 #include"Figura.h"
 
+#define  PI   3.14159265358979323846
+
 
 
 void crea_VAO_Vector(Figura* fig)
@@ -45,11 +47,84 @@ void crea_VAO_CP(Figura* fig)
 }
 
 
-void costruisci_cielo(Figura* fig);
+void costruisci_cielo(Figura* cielo, vec4 color_bot, vec4 color_top)
+{
+	//Inserimento vertici e colori
+	cielo->vertici.push_back(vec3(0.0, 0.0, 0.0));
+	cielo->colors.push_back(vec4(color_bot.r, color_bot.g, color_bot.b, color_bot.a));
+	cielo->vertici.push_back(vec3(1.0, 0.0, 0.0));
+	cielo->colors.push_back(vec4(color_bot.r, color_bot.g, color_bot.b, color_bot.a));
+	cielo->vertici.push_back(vec3(0.0, 1.0, 0.0));
+	cielo->colors.push_back(vec4(color_top.r, color_top.g, color_top.b, color_top.a));
+	cielo->vertici.push_back(vec3(1.0, 1.0, 0.0));
+	cielo->colors.push_back(vec4(color_top.r, color_top.g, color_top.b, color_top.a));
 
-void costruisci_prato(Figura* fig);
+	cielo->nv = cielo->vertici.size();
 
-void costruisci_sole(Figura* fig);
+	//Costruzione matrice di modellazione
+	cielo->Model = mat4(1.0);
+	cielo->Model = translate(cielo->Model, vec3(0.0, float(720) / 2, 0.0));
+	cielo->Model = scale(cielo->Model, vec3(float(1280), float(720) / 2, 1.0));
+}
+
+void costruisci_prato(Figura* prato, vec4 color_bot, vec4 color_top)
+{
+	//Inserimento vertici e colori
+	prato->vertici.push_back(vec3(0.0, 0.0, 0.0));
+	prato->colors.push_back(vec4(color_top.r, color_top.g, color_top.b, color_top.a));
+	prato->vertici.push_back(vec3(0.0, 1.0, 0.0));
+	prato->colors.push_back(vec4(color_top.r, color_top.g, color_top.b, color_top.a));
+	prato->vertici.push_back(vec3(1.0, 0.0, 0.0));
+	prato->colors.push_back(vec4(color_top.r, color_top.g, color_top.b, color_top.a));
+	prato->vertici.push_back(vec3(1.0, 1.0, 0.0));
+	prato->colors.push_back(vec4(color_bot.r, color_bot.g, color_bot.b, color_bot.a));
+
+	prato->nv = prato->vertici.size();
+
+	//Costruzione matrice di Modellazione
+	prato->Model = mat4(1.0);
+	prato->Model = scale(prato->Model, vec3(float(1280), float(720) / 2, 1.0));
+}
+
+void costruisci_sole(Figura* sole, vec4 color_radius, vec4 color_center, vec4 color_radius_alone, vec4 color_center_alone)
+{
+	int i;
+	float stepA = (2 * PI) / sole->nTriangles;
+	float t;
+
+
+	sole->vertici.push_back(vec3(0.0, 0.0, 0.0));
+	sole->colors.push_back(vec4(color_center.r, color_center.g, color_center.b, color_center.a));
+
+	for (i = 0; i <= sole->nTriangles; i++)
+	{
+		t = (float)i * stepA;
+
+		sole->vertici.push_back(vec3(cos(t), sin(t), 0.0));
+		sole->colors.push_back(vec4(color_radius.r, color_radius.g, color_radius.b, color_radius.a));
+	}
+
+	//Costruzione Alone
+	sole->vertici.push_back(vec3(0.0, 0.0, 0.0));
+	sole->colors.push_back(vec4(color_center_alone.r, color_center_alone.g, color_center_alone.b, color_center_alone.a));
+	for (i = 0; i <= sole->nTriangles; i++)
+	{
+		t = (float)i * stepA;
+
+		sole->vertici.push_back(vec3(2.0 * cos(t), 2.0 * sin(t), 0.0));
+		sole->colors.push_back(vec4(color_radius_alone.r, color_radius_alone.g, color_radius_alone.b, color_radius_alone.a));
+
+	}
+
+
+	sole->nv = sole->vertici.size();
+
+	//Costruzione matrice di Moellazione	
+	sole->Model = mat4(1.0);
+	sole->Model = translate(sole->Model, vec3(float(1280) * 0.5, float(720) * 0.8, 0.0));
+	sole->Model = scale(sole->Model, vec3(30.0, 30.0, 1.0));
+
+}
 
 void costruisci_luna(Figura* fig);
 
--- Daisy 'n' Steve/Figura.h
+++ Daisy 'n' Steve/Figura.h
@@ -29,11 +29,11 @@ void crea_VAO_Vector(Figura* fig);
 
 void crea_VAO_CP(Figura* fig);
 
-void costruisci_cielo(Figura* fig);
+void costruisci_cielo(Figura* cielo, vec4 color_bot, vec4 color_top);
 
-void costruisci_prato(Figura* fig);
+void costruisci_prato(Figura* prato, vec4 color_bot, vec4 color_top);
 
-void costruisci_sole(Figura* fig);
+void costruisci_sole(Figura* sole, vec4 color_radius, vec4 color_center, vec4 color_radius_alone, vec4 color_center_alone);
 
 void costruisci_luna(Figura* fig);
 
